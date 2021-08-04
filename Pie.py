84f92c106a32b5d273bc8f465a61a6cfce185326
trefalmadore <87436173+trefalmadore@users.noreply.github.com>
2021-08-04 14:32:07 +0100

    late change

--- Pie.py
+++ Pie.py
@@ -5,5 +5,5 @@ sizes = [ 10 , 20, 50 ,20,15,35]
 color =[ 'blue' , 'red' , 'purple', 'pink','orange','grey']
 explodes =(0,0,0,0,0,0.1)
 plt.pie( sizes, labels=label ,explode = explodes, colors = color)
-plt.title('Foods I want to  cook')
+plt.title('Food wich I like to eat')
 plt.show()
