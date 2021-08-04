f7a5820889632e700452b6c70c0b51367ada3892
trefalmadore <87436173+trefalmadore@users.noreply.github.com>
2021-08-04 12:02:29 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from matplotlib import pyplot as plt
+
+agesX = [20, 25,30, 35, 40, 45, 50,55, 60,65]
+
+salaryY = [18000, 20000,25000,28000,30000,35000,40000,45000,50000,55000]
+
+plt.plot(agesX, salaryY)
+
+plt.show()
